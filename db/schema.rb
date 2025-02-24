f08ee6587538408703823228c9456e2c06f9ae44
Ela <teodocio.am@gmail.com>
2025-02-24 12:57:58 -0600

    feat: migrate category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_24_151036) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_24_185302) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
